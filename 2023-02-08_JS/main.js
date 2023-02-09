4e9a002a3e638096f960564a31c53ce73575d3a0
Vilma <vilmail@protonmail.com>
2023-02-09 21:00:53 +0200

    added stone animation and little refactor

--- 2023-02-08_JS/main.js
+++ 2023-02-08_JS/main.js
@@ -1,94 +1,84 @@
-// const answerElemA = document.getElementById("answer-a");
-// const answerElemB = document.getElementById("answer-b");
-const buttonElemA = document.getElementById("start-btn-a");
-const boardElemA = document.getElementById("board-a");
-const counterElemA = document.getElementById("hit-counter-a");
-const buttonElemB = document.getElementById("start-btn-b");
-const boardElemB = document.getElementById("board-b");
-const counterElemB = document.getElementById("hit-counter-b");
-let running = false;
+const a = {
+  buttonEl: document.getElementById("start-btn-a"),
+  boardEl: document.getElementById("board-a"),
+  counterEl: document.getElementById("hit-counter-a"),
+  nailsCount: 5,
+  nailLength: 85, //mm
+  hitMin: 5, //mm
+  hitMax: 20, //mm
+  hitFailChance: 0, //%
+};
+const b = {
+  buttonEl: document.getElementById("start-btn-b"),
+  boardEl: document.getElementById("board-b"),
+  counterEl: document.getElementById("hit-counter-b"),
+  nailsCount: 5,
+  nailLength: 85, //mm
+  hitMin: 20, //mm
+  hitMax: 30, //mm
+  hitFailChance: 50, //%
+};
+//adjust board style
+a.boardEl.style.height = `${a.nailLength}px`;
+a.boardEl.style.marginTop = `${a.nailLength + 50}px`;
+b.boardEl.style.height = `${b.nailLength}px`;
+b.boardEl.style.marginTop = `${b.nailLength + 50}px`;
+let running = false; //to stop user interactions when loop with awaits, as it stops execution
 
-buttonElemA.addEventListener("click", () => {
+a.buttonEl.addEventListener("click", () => {
   if (!running) {
-    countLightHits(5, 85);
+    countHits(a);
   }
 });
-
-buttonElemB.addEventListener("click", () => {
+b.buttonEl.addEventListener("click", () => {
   if (!running) {
-    countHeavyHits(5, 85);
+    countHits(b);
   }
 });
-async function countLightHits(nailsCount, nailLength) {
+async function countHits(arg) {
+  let { nailsCount, nailLength, hitMin, hitMax, hitFailChance, boardEl, counterEl } = arg;
   running = true;
-  boardElemA.innerHTML = "";
-  const hitMin = 5;
-  const hitMax = 20;
+  ///reset
+  boardEl.innerHTML = "";
+  counterEl.innerHTML = "";
+  ///
   let hitsCount = 0;
   for (let i = 0; i < nailsCount; i++) {
-    let el = addNailElement(i);
-    let stone = el.getElementsByClassName("stone")[0];
-    let hitTextEl = el.getElementsByClassName("hit-text")[0];
-    let leftTextEl = el.getElementsByClassName("left-text")[0];
-    boardElemA.appendChild(el);
-
-    await wait(1000); //to slow down visualisation
-    let j = nailLength;
-    leftTextEl.textContent = j;
-    await wait(300); //to slow down visualisation
-    while (j >= 0) {
+    let nailEl = createNailElement(i);
+    nailEl.style.height = `${nailLength}px`;
+    let stoneEl = nailEl.getElementsByClassName("stone")[0];
+    let hitTextEl = nailEl.getElementsByClassName("hit-text")[0];
+    let leftTextEl = nailEl.getElementsByClassName("left-text")[0];
+    boardEl.appendChild(nailEl);
+    let nailOutside = nailLength;
+    leftTextEl.textContent = nailOutside;
+    await wait(900); //to slow down visualisation
+    while (nailOutside >= 0) {
+      stoneEl.classList.add("active");
       hitsCount++;
-      let hit = rand(hitMin, hitMax);
-      j -= hit;
-      hitTextEl.textContent = hit;
-      leftTextEl.textContent = j;
-      el.style.top = `-${j}px`;
-      counterElemA.textContent = hitsCount;
-      await wait(300); //to slow down visualisation
-    }
-    el.style.top = `0px`;
-    el.classList.remove("active");
-  }
-  running = false;
-}
-async function countHeavyHits(nailsCount, nailLength) {
-  running = true;
-  boardElemB.innerHTML = "";
-  const hitMin = 20;
-  const hitMax = 30;
-  let hitsCount = 0;
-  for (let i = 0; i < nailsCount; i++) {
-    let el = addNailElement(i);
-    let stone = el.getElementsByClassName("stone")[0];
-    let hitTextEl = el.getElementsByClassName("hit-text")[0];
-    let leftTextEl = el.getElementsByClassName("left-text")[0];
-    boardElemB.appendChild(el);
-
-    await wait(1000); //to slow down visualisation
-    let j = nailLength;
-    leftTextEl.textContent = j;
-    await wait(300); //to slow down visualisation
-    while (j >= 0) {
-      hitsCount++;
-      let hitOk = rand(0, 1);
       let hit = 0;
-      if (hitOk) {
+      if (!failureByChance(hitFailChance)) {
         hit = rand(hitMin, hitMax);
+      } else {
+        stoneEl.classList.add("failure");
       }
-      j -= hit;
-      hitTextEl.textContent = hit;
-      leftTextEl.textContent = j;
-      el.style.top = `-${j}px`;
-      counterElemB.textContent = hitsCount;
-      await wait(300); //to slow down visualisation
+      nailOutside -= hit;
+      hitTextEl.textContent = hit; //how much did current hit pushed nail in to the board
+      leftTextEl.textContent = nailOutside >= 0 ? nailOutside : 0; //if negative, show 0 (how much nail left(outside))
+      nailEl.style.top = `-${nailOutside >= 0 ? nailOutside : 0}px`; //visual
+      counterEl.textContent = hitsCount; //counting hits
+      await wait(900); //to slow down visualisation
+      console.log("removing");
+      stoneEl.classList.remove("active");
+      stoneEl.classList.remove("failure");
+      await wait(100);
     }
-    el.style.top = `0px`;
-    el.classList.remove("active");
+    nailEl.classList.remove("active");
   }
   running = false;
 }
 
-function addNailElement(nth) {
+function createNailElement(nth) {
   let hitText = document.createElement("span");
   hitText.classList.add("hit-text");
   let leftText = document.createElement("span");
@@ -97,19 +87,17 @@ function addNailElement(nth) {
   stoneEl.classList.add("stone");
   stoneEl.appendChild(hitText);
   stoneEl.appendChild(leftText);
-  let el = document.createElement("div");
-  el.classList.add("nail");
-  el.classList.add("active");
-  el.style.left = `${nth * 50 + 10}px`;
-  el.appendChild(stoneEl);
-  return el;
+  let nailEl = document.createElement("div");
+  nailEl.classList.add("nail");
+  nailEl.classList.add("active");
+  nailEl.style.left = `${nth * 50 + 10}px`;
+  nailEl.appendChild(stoneEl);
+  return nailEl;
 }
 
 function rand(min, max) {
   min = Math.ceil(min);
-
   max = Math.floor(max);
-
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function wait(miliseconds) {
@@ -118,4 +106,20 @@ function wait(miliseconds) {
       resolve();
     }, miliseconds);
   });
+}
+function failureByChance(failChancePercentage) {
+  if (failChancePercentage === 50) {
+    return rand(0, 1);
+  } else if (failChancePercentage === 0) {
+    return 0;
+  } else if (failChancePercentage === 100) {
+    return 1;
+  } else {
+    let chance = rand(1, 100);
+    if (chance <= failChancePercentage) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
