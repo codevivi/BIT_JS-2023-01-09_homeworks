a4093ef3d1a597793c43b194a1fd94117ad4c241
Vilma <vilmail@protonmail.com>
2023-02-09 15:20:11 +0200

    second pard feb8

--- 2023-02-08_JS/main.js
+++ 2023-02-08_JS/main.js
@@ -1,5 +1,5 @@
-const answerElemA = document.getElementById("answer-a");
-const answerElemB = document.getElementById("answer-b");
+// const answerElemA = document.getElementById("answer-a");
+// const answerElemB = document.getElementById("answer-b");
 const buttonElemA = document.getElementById("start-btn-a");
 const boardElemA = document.getElementById("board-a");
 const counterElemA = document.getElementById("hit-counter-a");
@@ -11,13 +11,16 @@ buttonElemA.addEventListener("click", () => {
   countLightHits(5, 85);
 });
 
+buttonElemB.addEventListener("click", () => {
+  countHeavyHits(5, 85);
+});
 async function countLightHits(nailsCount, nailLength) {
   boardElemA.innerHTML = "";
   const hitMin = 5;
   const hitMax = 20;
   let hitsCount = 0;
   for (let i = 0; i < nailsCount; i++) {
-    let el = addNailElement(boardElemA, i);
+    let el = addNailElement(i);
     let stone = el.getElementsByClassName("stone")[0];
     let hitTextEl = el.getElementsByClassName("hit-text")[0];
     let leftTextEl = el.getElementsByClassName("left-text")[0];
@@ -41,8 +44,42 @@ async function countLightHits(nailsCount, nailLength) {
     el.classList.remove("active");
   }
 }
+async function countHeavyHits(nailsCount, nailLength) {
+  boardElemB.innerHTML = "";
+  const hitMin = 20;
+  const hitMax = 30;
+  let hitsCount = 0;
+  for (let i = 0; i < nailsCount; i++) {
+    let el = addNailElement(i);
+    let stone = el.getElementsByClassName("stone")[0];
+    let hitTextEl = el.getElementsByClassName("hit-text")[0];
+    let leftTextEl = el.getElementsByClassName("left-text")[0];
+    boardElemB.appendChild(el);
 
-function addNailElement(board, nth) {
+    await wait(1000); //to slow down visualisation
+    let j = nailLength;
+    leftTextEl.textContent = j;
+    await wait(300); //to slow down visualisation
+    while (j >= 0) {
+      hitsCount++;
+      let hitOk = rand(0, 1);
+      let hit = 0;
+      if (hitOk) {
+        hit = rand(hitMin, hitMax);
+      }
+      j -= hit;
+      hitTextEl.textContent = hit;
+      leftTextEl.textContent = j;
+      el.style.top = `-${j}px`;
+      counterElemB.textContent = hitsCount;
+      await wait(300); //to slow down visualisation
+    }
+    el.style.top = `0px`;
+    el.classList.remove("active");
+  }
+}
+
+function addNailElement(nth) {
   let hitText = document.createElement("span");
   hitText.classList.add("hit-text");
   let leftText = document.createElement("span");
