67c89ecc05ca5816cfa20db9656ae05380d18635
Vilma <vilmail@protonmail.com>
2023-04-01 20:16:15 +0300

    up to test 6

--- 2023-03-30-js-tests/testas-6/main.js
+++ 2023-03-30-js-tests/testas-6/main.js
@@ -4,3 +4,15 @@ function rand(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+// Sukurti aplikaciją su mygtuku “Reset” ir į dvi dalis apačioje
+// padalintą ekraną. Paspaudus mygtuką “Reset” kairėje pusėje atsiranda
+// rand 5, 15 kiekis apskritimų sunumeruoti nuo 1 eilės tvarka, o dešinė
+// pusė tuščia. Paspaudus ant bet kokio apskritimo kairėje pusėje jis
+// atsiranda dešinėje pusėje, o kairėje išnyksta. Taip iš kairės į dešinę
+// turi būti galima perkelti visus apskritimus. Paspaudus ant dešinėje
+// pusėje esančio apskritimo jis sugrįžta į kairę pusę ir t.t.
+// Apskritimai abiejuose pusėse turi rikiuotis pagal eilės numerį
+// didėjimo tvarka. Tvarka turi išlikti dėliojant apskritimus iš vienos
+// pusės į kitą.
+// Vėl paspaudus mygtuką “Reset” seni apskritimai išnyksta, kairėje
+// pusėje vėl atsiranda naujas apskritimų kiekis, dešinė pusė- tuščia.
