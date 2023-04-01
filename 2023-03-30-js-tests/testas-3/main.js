f72b7b99760cdae46b9b4dc7edca1a31d0d8f41b
Vilma <vilmail@protonmail.com>
2023-04-01 17:38:45 +0300

    testas #3 done

--- 2023-03-30-js-tests/testas-3/main.js
+++ 2023-03-30-js-tests/testas-3/main.js
@@ -10,20 +10,36 @@ const cats = ["Murka", "Rainius", "Meilutė", "Bosas", "Dičkis"];
 // 1. Prie masyvo bitGirls pradžios (ne galo!) pridėkite Nausėdą (po
 // pridėjimo bitGirls turės 6 elementus).
 
+bitGirls.unshift("Nausėda");
+console.log(bitGirls);
+
 // 2. Sukurkite naują masyvą bitCats iš masyvo cats taip kad naujasis
 // masyvas turėtų elementus iš mažų, dviejų narių, masyviukų: pirmas
 // narys katinuko vardas, antras - katinuko svoris - atsitiktinės
 // tekstinės reikšmės - “storas” arba “normalus”.
 
+const bitCats = cats.map((cat) => [cat, rand(0, 1) ? "storas" : "normalus"]);
+console.log(bitCats);
+
 // 3. bitCats masyve suskaičiuokite kiek yra storų ir normalių
 // katinukų.
+
+let storiCount = 0;
+let normalusCount = 0;
+bitCats.forEach((cat) => (cat[1] === "storas" ? storiCount++ : normalusCount++));
+console.log(`storų: ${storiCount}, normalių: ${normalusCount}`);
 
 // 4. Išrūšiuokite gautą bitsCats masyvą pagal antrą katinuko vardo
 // raidę, abėcėlės tvarka (ne pagal visą vardą, o tik pagal antrą
 // raidę) Tokiu būdu katinukas vardu Rainius pagal antrą “a” raidę
 // turėtų atsidurti viršuje.
 
+console.log(bitCats.sort((a, b) => a[0][1].localeCompare(b[0][1])));
+
 // 5. (BOSO lygis) Iš masyvų bitGirls ir bitCats padarykite masyvą
 // happyCats, kurio elementai būtų masyvai iš mergaitės vardo ir
 // katinuko vardo. Nausėdai katinuko neduokit (nes neužteks) ir
 // Nausėdai vietoj katinuko priskirkite stringą “Barsukas”.
+
+const happyCats = bitGirls.map((name, ind) => [name, ind === 0 ? "Barsukas" : bitCats[ind - 1][0]]);
+console.log(happyCats);
